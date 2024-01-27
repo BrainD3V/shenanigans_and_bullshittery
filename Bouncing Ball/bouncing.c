d8babcb74594aa9046a0c3debae08fe49e0416f1
BrainD3V <157586120+BrainD3V@users.noreply.github.com>
2024-01-27 08:09:19 +0100

    Updated Bouncing.c
    In this new version:
    1) It's possible to add an external new force
    2) Input thread works more or less                //I needed to get some objects global instead of declaring them in the main
    
    It isn't possible yet:
    3) To insert any command while keeping the animation going
    4) To add new balls

--- Bouncing Ball/bouncing.c
+++ Bouncing Ball/bouncing.c
@@ -6,6 +6,8 @@
 #include <pthread.h>
 
 #define NUMTHREADS 2
+#define SPD        125
+//structs
 struct vect2{
 	int 	x;
         int	y;
@@ -22,6 +24,7 @@ struct point{
 struct display{
 	int w, h;
 	char** screen;
+	char*  cmd;
 };
 
 struct t_args{
@@ -29,8 +32,12 @@ struct t_args{
 	struct point* 		pnt;
 };
 
-// constants
-const int CMDLEN = 25;
+// globals
+const int CMDLEN = 100;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+struct display disp;
+struct point pnt;
+
 // // point const
 const int acc_decay = 0;
 struct vect2 grav = {.x = 0, .y = -1};
@@ -73,11 +80,11 @@ void new_state(struct point* p, struct display* d){
 	if(p->pos.x + p->vel.x >= d->w || p->pos.x + p->vel.x <= 0) {
 		p->vel.x *= -1;
 
-		p->vel.x -= p->pos.x;
+	//	p->vel.x -= p->pos.x;
 		if(p->pos.x <= 0){
-			p->pos.x = 0;
+			p->pos.x = 1;
 		} else if (p->pos.x + p->vel.x >= d->w) {
-			p->pos.x = d->w;
+			p->pos.x = d->w - 1;
 		}
 	}
 
@@ -85,19 +92,20 @@ void new_state(struct point* p, struct display* d){
 		p->vel.y *= -1;
 		p->vel.y -= p->pos.y;
 		if(p->pos.y + p->vel.y <= 0){
-			p->pos.y = 0;
+			p->pos.y = 1;
 		} else if(p->pos.y + p->vel.y >= d->h){
-			p->pos.y = d->h;
+			p->pos.y = d->h - 2;
 		}
 	}
 		
-
 	add_vect(&(p->pos), p->vel.x, p->vel.y);
 	add_vect(&(p->vel), p->acc.x, p->acc.y);
 	add_vect(&(p->acc), acc_decay, acc_decay);
-	mod_vect(&(p->acc), 0, grav.y);
+	add_vect(&(p->acc), 0, grav.y);
        	cap_vect(&(p->vel), -5, -5, 1);
-	cap_vect(&(p->acc), -3, -3, 1);	
+	cap_vect(&(p->vel),  5,  5, 0);
+	cap_vect(&(p->acc), -3, -3, 1);
+	cap_vect(&(p->acc),  3,  3, 0);
 }
 
 
@@ -107,13 +115,18 @@ void screen_init(struct display* d, int wid, int hei){
 	d->w = wid;
 	d->h = hei;
 	d->screen = (char**) malloc(hei*sizeof(char*));
+	d->cmd    = (char*) malloc(CMDLEN*sizeof(char));
+	strcpy(d->cmd, "Input> ");
 	for(int i = 0; i < hei; i++){
 		d->screen[i] = (char*) malloc((wid+1)*sizeof(char));
-		for(int j = 0; j < wid; j++){
+		d->screen[i][0] = '|';
+		for(int j = 1; j < wid-1; j++){
 			d->screen[i][j] = ' ';
 		}
-		d->screen[i][wid] = '\n';
+		d->screen[i][(d->w)-1] = '|';
+		d->screen[i][(d->w)] = '\n';
 	}
+
 }
 
 void screen_show(struct display* d, struct point* p){
@@ -125,46 +138,68 @@ void screen_show(struct display* d, struct point* p){
 	
 	int wid = d->w;
 	int hei = d->h;
-
+	
+	
 	d->screen[hei-lpy][wid-lpx] = ' ';
 	d->screen[hei-poy][wid-pox] = p->symbol;
-
+	
 	for(int y = 0; y < d->h; y++){
 		printf("%s", d->screen[y]);
 	}
+	for(int x = 0; x < d->w; x++) printf("=");
+	printf("\n%s", d->cmd);
+	printf("\n");
 }
 
 //Threads 
 void* anim_thread(void *args){
 	struct t_args *dispnt = args;
-	struct display disp = *dispnt->d;
-	struct point   pnt  = *dispnt->pnt;
 	while(1){
+		
+		pthread_mutex_lock(&mutex);
+		
 		screen_show(&disp, &pnt);
 		new_state(&pnt, &disp);
-		usleep(250*1000);
-		printf("point state::> x= %d\t y= %d\t vx=%d\t vy=%d\n", pnt.pos.x, pnt.pos.y, pnt.vel.x, pnt.vel.y);	
+		
+		pthread_mutex_unlock(&mutex);
+		
+		usleep(SPD*1000);
+		system("clear");
+		printf("point state::> x= %d\t y= %d\t vx=%d\t vy=%d\t ax= %d\t ay= %d\n", pnt.pos.x, pnt.pos.y, pnt.vel.x, pnt.vel.y);
 	}
 	free(dispnt);
 }
 
 void* input_thread(void* args){
 	struct t_args *dispnt = args; 
-	struct point pnt = *dispnt->pnt;
 	char cmd[CMDLEN];
-	char fun[3];
+	char fun[4];
 	int  x;
 	int  y;
+	
 	while(1){
-		system("clear");
-		printf("Input>");
-		scanf("%s", cmd);
-		sscanf(cmd, "%s %d %d", fun, x, y);
-		if(strcmp(fun, "app") == 0){
-			add_vect(&pnt.acc, x, y);
-		} else if (strcmp(fun, "kll") == 0){
-			break;
+		strcpy(disp.cmd, "Input> ");
+		if( scanf("%s", fun) > 0){
+			pthread_mutex_lock(&mutex);
+			sscanf(cmd, "%s", fun);
+			strcat(disp.cmd, fun);
+			pthread_mutex_unlock(&mutex);
 		} else {
+			continue;
+		}
+
+		if(strcmp(fun, "app") == 0){
+			pthread_mutex_lock(&mutex);
+			strcpy(disp.cmd, "App listened.\n x: ");
+			scanf("%d", &x);
+			strcpy(disp.cmd, "y: ");
+			scanf("%d", &y);
+			add_vect(&pnt.acc, x, y);
+			pthread_mutex_unlock(&mutex);
+		} else if (strcmp(fun, "kll") == 0){
+			exit(EXIT_FAILURE);
+		} else {
+			printf("%s\n", cmd);
 			printf("Comando non riconosciuto\n");
 		}	
 	}
@@ -183,21 +218,27 @@ int main(int argc, char** argv){
 	int ppy = atoi(argv[4]);
 	
 	printf("Creo il display\n");	
-	struct display disp;
 	screen_init(&disp, dwid, dhei);
-	printf("Creo il punto\n");
-	struct point pnt;
+	printf("Creo il punto\n");	
 	point_create(&pnt, ppx, ppy);
-
+	
+	printf("Inizializzo gli argomenti per i thread\n");
 	struct t_args *args = malloc(sizeof(struct t_args*));
 	args->d = &disp;
 	args->pnt = &pnt;
 
 	pthread_t threads[NUMTHREADS];
 	
-	pthread_create(&threads[0], NULL, anim_thread, args);
-	pthread_create(&threads[1], NULL, input_thread, args);
-
+	if( pthread_create(&threads[0], NULL, anim_thread, args) != 0){
+		fprintf(stderr, "Errore nella creazione del thread animato: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	printf("Creato thread per l'animazione\n");
+	if( pthread_create(&threads[1], NULL, input_thread, args) != 0){
+		fprintf(stderr, "Errore nella creazione del thread input: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	printf("Creato thread per l'input.\n");
 	pthread_join(threads[0], NULL);
 	pthread_join(threads[1], NULL);
 	
